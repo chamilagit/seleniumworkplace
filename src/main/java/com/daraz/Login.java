804378e63d25c757550e1992ef16703566c5beab
chamilagit <84230552+chamilagit@users.noreply.github.com>
2022-11-09 09:01:52 +0530

    Update Login page

--- src/main/java/com/daraz/Login.java
+++ src/main/java/com/daraz/Login.java
@@ -1,15 +1,37 @@
 package com.daraz;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
-    public static void main(String[] args) {
+        WebDriver webDriver;
+        String LOGINLABLE = "//header/div[1]/div[1]/div[1]/div[1]/div[2]/ul[1]/li[4]";
+        String EMAIL = "//*[@id=\"username\"]";
+        String PASSWORD = "//*[@id=\"password\"]";
 
-        String LOGINLABLE = "//a[contains(text(),'LOGIN')]";
-        String relativexpath = "//input[@name='q']";
+        String LOGINGBUTTON ="//*[@id=\"post-8295\"]/div/div/div[2]/div/div/div/div/div[1]/form/p[3]/button";
 
-        System.setProperty("webdriver.chrome.driver", "F:\\SeleniumTesting\\chromedriver.exe");
-        WebDriver webDriver = new ChromeDriver();
+        String HOMEBUTTON ="//*[@id=\"site-header\"]/div[1]/div/div/div/div[1]/div[1]/div/a/img";
 
-    }
+
+        public Login(WebDriver webDriver){
+            this.webDriver=webDriver;
+        }
+        public void clickLoginlink(){
+                webDriver.findElement(By.xpath(LOGINLABLE)).click();
+        }
+        public void typeusername(){
+                webDriver.findElement(By.xpath(EMAIL)).sendKeys("chamilawasi");
+        }
+        public void typepassword(){
+                webDriver.findElement(By.xpath(PASSWORD)).sendKeys("chamila@123");
+        }
+        public void clickloginbutton(){
+                webDriver.findElement(By.xpath(LOGINGBUTTON)).click();
+        }
+
+        public void visitStore(){
+                webDriver.findElement(By.xpath(HOMEBUTTON)).click();
+        }
 }
